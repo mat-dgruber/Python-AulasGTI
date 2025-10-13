2731e93ffce92be3ea10f71106b5a6b5d28cd95a
Matheus Diniz <matheus.gruber123@gmail.com>
2025-10-13 09:15:01 -0300

    Base

---
+++ main.py
@@ -0,0 +1,11 @@
+id_usuario = 1817
+nome_usuario = "Matheus Diniz"
+email_usuario = "matheus.diniz@cpb.com.br"
+ativo = True
+tags = ["premium", "email_confirmado", "notAdmin"]
+
+print(f"ID: {id_usuario} (tipo: {type(id_usuario)})")
+print(f"Nome: {nome_usuario} (tipo: {type(nome_usuario)})")
+print(f"Email: {email_usuario} (tipo: {type(email_usuario)})")
+print(f"Ativo: {ativo} (tipo: {type(ativo)})")
+print(f"Tags: {tags} (tipo: {type(tags)})")
